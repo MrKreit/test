97f42dfc1a77cddf19eaa912cf5392de4a8235e3
Roman Luchenkov <lucencov.roman@gmail.com>
2023-10-31 22:45:37 +0100

    make addition calculator

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 public class Hello {
-
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a + b);
   }
-
-
 }
